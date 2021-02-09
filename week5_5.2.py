109b8a9327791fe72e56c0bcf799f5694ac0ec2a
Watcharachat Asana <watcharachat.a@kkumail.com>
2021-02-09 17:50:46 +0700

    upload

---
+++ week5_5.2.py
@@ -0,0 +1,10 @@
+class shop :
+    def __init__(self,name,item,price,amount):
+        self.name = name
+        self.item = item
+        self.price = price
+        self.amount = amount
+
+    @classmethod
+    def
+
